a1491b273b031d60eaf5e3106a68c022ce447a18
Cab Martinez Rodolfo <recm_95@hotmail.com>
2016-05-25 13:03:00 -0500

    Simulador Contador Manual

---
+++ src/simuladorcontadormanual/SimuladorContadorManual.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladorcontadormanual;
+
+import gui.Visual;
+
+/**
+ *
+ * @author RodolfoECM
+ */
+public class SimuladorContadorManual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Visual ami=new Visual();
+       ami.setLocationRelativeTo(null);
+       ami.setVisible(true);
+    }
+    
+}
